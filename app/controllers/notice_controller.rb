f16530c7d26c0c30ad6d45e2f4dad41ae656e580
Rabea Gleissner <rabeagleissner@gmail.com>
2015-03-26 10:09:25 +0000

    method to partly save opening hours for individual days

--- app/controllers/notice_controller.rb
+++ app/controllers/notice_controller.rb
@@ -12,9 +12,9 @@ class NoticeController < ApplicationController
 
     @icao_code = icao_code(input_array)
 
-    @mon = monday(input_array)
+    weekday(input_array)
 
-    @notice = Notice.new(icao_code: @icao_code, mon: @mon)
+    @notice = Notice.new(icao_code: @icao_code, mon: @mon, tue: @tue, wed: @wed, thu: @thu, fri: @fri, sat: @sat, sun: @sun)
     @notice.save
 
     redirect_to results_path
@@ -39,13 +39,38 @@ class NoticeController < ApplicationController
   end
 
 # Method to find Monday opening times
-  def monday(input_array)
-    array = split_array(input_array)
+  def weekday(input_array)
+    array = split_array(input_array).flatten
 
-    monday = array.select {|x| x.include?("E)")}.flatten
-    # n = /\AMON/
-    index = monday.index("MON")
-    monday[index+1]  
+    # monday = array.select {|x| x.include?("E)")}
+  if array.include? "MON"
+    index = array.index("MON")
+    @mon = array[index+1]  
+  end
+  if array.include? "TUE"
+    index = array.index("TUE")
+    @tue = array[index+1]  
+  end
+  if array.include? "WED"
+    index = array.index("WED")
+    @wed = array[index+1]  
+  end
+  if array.include? "THU"
+    index = array.index("THU")
+    @thu = array[index+1]  
+  end
+  if array.include? "FRI"
+    index = array.index("FRI")
+    @fri = array[index+1]  
+  end
+  if array.include? "SAT"
+    index = array.index("SAT")
+    @sat = array[index+1]  
+  end
+  if array.include? "SUN"
+    index = array.index("SUN")
+    @sun = array[index+1]  
+  end
       
   end
 
